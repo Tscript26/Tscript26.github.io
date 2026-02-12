1c4e9d380319b5619175b0e63d97100d3d5ad997
Tscript26 <Tscript26@users.noreply.github.com>
2026-02-12 09:47:44 +0000

    Deploying from private repo: f8b661d4a16cfb405c7ced9287d772ed7f389ec5 Tscript26/my_blog@f8b661d4a16cfb405c7ced9287d772ed7f389ec5

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1770888729',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1770889662',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
